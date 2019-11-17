abf20df935d1d68c8eb498afb641d6fa593a686e
Artur Quirino <arturquirino6@gmail.com>
2019-11-17 20:49:05 -0300

    Creation of the API

---
+++ Backend/RealManager/RealManager/Controllers/MatchController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MatchController : ControllerBase
+    {
+        public MatchController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return null;
+        }
+    }
+}
