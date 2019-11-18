c50d9563e999f7c697ea90c2f42e9341fcf9db28
Artur Quirino <arturquirino6@gmail.com>
2019-11-17 21:26:41 -0300

    simplest algorithm for match moves

---
+++ Backend/RealManager/RealManager/Domain/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealManager.Domain
+{
+    public class Player
+    {
+        public int Pace { get; set; }
+        public int Shoot { get; set; }
+        public int Pass { get; set; }
+        public int Drible { get; set; }
+        public int Defence { get; set; }
+        public int Physical { get; set; }
+    }
+}
