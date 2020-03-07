dbb3ffc57243188863fd5663eadaec751354ae5e
Artur Quirino <artur.pquirino@gmail.com>
2020-03-07 12:46:33 -0300

    Add theme for the application

--- Frontend/real-manager/src/app/app.module.ts
+++ Frontend/real-manager/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { MaterialModule } from './modules/material.module';
+import { MaterialModule } from './shared/modules/material.module';
 
 @NgModule({
   declarations: [
