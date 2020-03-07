addc7c5ef3703e9f42497feabb087653f1c36021
Artur Quirino <artur.pquirino@gmail.com>
2020-03-07 13:02:19 -0300

    Create signup component

--- Frontend/real-manager/src/app/app.module.ts
+++ Frontend/real-manager/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MaterialModule } from './shared/modules/material.module';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
--- Frontend/real-manager/src/app/login/login.component.ts
+++ Frontend/real-manager/src/app/login/login.component.ts
@@ -18,6 +18,4 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
-
-
 }
