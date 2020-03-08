36b3f125c48ec6402e3d6199f6c1f3c6e399acd3
Artur Quirino <artur.pquirino@gmail.com>
2020-03-07 21:24:20 -0300

    Remove unused variables

--- Backend/RealManager/RealManager/Services/MatchService.cs
+++ Backend/RealManager/RealManager/Services/MatchService.cs
@@ -2,65 +2,20 @@
 using RealManager.Repositories.Interfaces;
 using RealManager.Services.Interfaces;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace RealManager.Services
 {
     public class MatchService : IMatchService
     {
         private readonly ITeamRepository _teamRepository;
-        private readonly IPlayerRepository _playerRepository;
-        public MatchService(ITeamRepository teamRepository, IPlayerRepository playerRepository){
+        public MatchService(ITeamRepository teamRepository){
             _teamRepository = teamRepository;
-            _playerRepository = playerRepository;
         }
         public bool RunMatchEvent(int time)
         {
             return true;
-            // var attackPlayer = TeamA.Players.FirstOrDefault(p => p.Position == Position.ATA);
-            // var defencePlayer = TeamB.Players.FirstOrDefault(p => p.Position == Position.ATA);
-            // var goalKeeperPlayer = TeamB.Players.FirstOrDefault(p => p.Position == Position.ATA);
 
-            // int dribleSuccessChance = attackPlayer.Drible + attackPlayer.Pace / 2 - 15 * time / attackPlayer.Physical;
-            // var randomNumber = new Random();
-
-            // var realDrible = randomNumber.Next(0, 150);
-
-            // var ableToDrible = realDrible > 150 - dribleSuccessChance;
-
-            // if (!ableToDrible) return false;
-
-
-            // int tackleSucessChance = defencePlayer.Defence + defencePlayer.Pace / 2 - 15 * time / defencePlayer.Physical;
-
-            // var realTackle = randomNumber.Next(0, 150);
-
-            // var ableToTackle = realTackle > 150 - tackleSucessChance;
-
-            // if (ableToTackle) return false;
-
-
-            // int shootOnTargetChance = attackPlayer.Shoot - 15 * time / attackPlayer.Physical;
-
-            // var realShoot = randomNumber.Next(0, 100);
-
-            // var ableToShootOnTarget = realShoot > 100 - shootOnTargetChance;
-
-            // if (!ableToShootOnTarget) return false;
-
-
-
-            // int defenceSucessChance = goalKeeperPlayer.Defence + goalKeeperPlayer.Pace / 2 - 5 * time / goalKeeperPlayer.Physical;
-
-            // var realDefence = randomNumber.Next(0, 150);
-
-            // var ableToDefence = realDefence > 150 - defenceSucessChance;
-
-            // if (ableToDefence) return false;
-
-            // return true;
         }
 
         public Match RunFriendly(Guid homeTeamId, Guid awayTeamId)
