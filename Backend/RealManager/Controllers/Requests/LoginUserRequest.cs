a1633f0d40fb241b8e378f8dd16ac93e8ce733fc
Artur Quirino <artur.pquirino@gmail.com>
2020-04-05 16:00:30 -0300

    Add API project

---
+++ Backend/RealManager/Controllers/Requests/LoginUserRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealManager.Controllers.Requests
+{
+    public class LoginUserRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
