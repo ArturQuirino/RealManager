7ac23be5b123b30304391e28d788ac89a7e6503e
Artur Quirino <artur.pquirino@gmail.com>
2020-04-10 20:04:40 -0300

    Add league table

--- Frontend/src/app/league/league.component.ts
+++ Frontend/src/app/league/league.component.ts
@@ -1,5 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface TeamLeague {
+  position: number;
+  teamName: string;
+  matches: number;
+  points: number;
+  won: number;
+  drawn: number;
+  lost: number;
+  goalsFor: number;
+  goalsAgainst: number;
+  goalDiference: number;
+}
+
+const LEAGUE_TABLE_DATA: TeamLeague[] = [
+  {position: 1, teamName: 'The best team', matches: 10, points: 30, drawn: 0,
+  won: 10, lost: 0, goalsFor: 30, goalsAgainst: 10, goalDiference: 20},
+  {position: 1, teamName: 'The best team', matches: 10, points: 30, drawn: 0,
+  won: 10, lost: 0, goalsFor: 30, goalsAgainst: 10, goalDiference: 20},
+  {position: 1, teamName: 'The best team', matches: 10, points: 30, drawn: 0,
+  won: 10, lost: 0, goalsFor: 30, goalsAgainst: 10, goalDiference: 20},
+  {position: 1, teamName: 'The best team', matches: 10, points: 30, drawn: 0,
+  won: 10, lost: 0, goalsFor: 30, goalsAgainst: 10, goalDiference: 20},
+  {position: 1, teamName: 'The best team', matches: 10, points: 30, drawn: 0,
+  won: 10, lost: 0, goalsFor: 30, goalsAgainst: 10, goalDiference: 20},
+  {position: 1, teamName: 'The best team', matches: 10, points: 30, drawn: 0,
+  won: 10, lost: 0, goalsFor: 30, goalsAgainst: 10, goalDiference: 20},
+  {position: 1, teamName: 'The best team', matches: 10, points: 30, drawn: 0,
+  won: 10, lost: 0, goalsFor: 30, goalsAgainst: 10, goalDiference: 20},
+  {position: 1, teamName: 'The best team', matches: 10, points: 30, drawn: 0,
+  won: 10, lost: 0, goalsFor: 30, goalsAgainst: 10, goalDiference: 20},
+  {position: 1, teamName: 'The best team', matches: 10, points: 30, drawn: 0,
+  won: 10, lost: 0, goalsFor: 30, goalsAgainst: 10, goalDiference: 20},
+  {position: 1, teamName: 'The best team', matches: 10, points: 30, drawn: 0,
+  won: 10, lost: 0, goalsFor: 30, goalsAgainst: 10, goalDiference: 20},
+];
+
+
 @Component({
   selector: 'app-league',
   templateUrl: './league.component.html',
@@ -7,6 +44,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeagueComponent implements OnInit {
 
+  displayedColumns: string[] = ['position', 'teamName', 'matches', 'won', 'drawn',
+  'lost', 'goalsFor', 'goalsAgainst', 'goalDiference', 'points'];
+  leagueDataSource = LEAGUE_TABLE_DATA;
   constructor() { }
 
   ngOnInit(): void {
--- Frontend/src/app/shared/modules/material.module.ts
+++ Frontend/src/app/shared/modules/material.module.ts
@@ -2,7 +2,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [],
@@ -11,6 +12,7 @@ import {MatButtonModule} from '@angular/material/button';
     ReactiveFormsModule,
     MatInputModule,
     MatButtonModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [],
@@ -19,6 +21,7 @@ import {MatButtonModule} from '@angular/material/button';
     ReactiveFormsModule,
     MatInputModule,
     MatButtonModule,
+    MatTableModule,
   ]
 })
 export class MaterialModule { }
