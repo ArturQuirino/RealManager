04da6ef95d16cd973e33b3f5f8f16c369231a23f
Artur Quirino <artur.pquirino@gmail.com>
2020-04-20 15:17:58 -0300

    Create Matches page

--- Frontend/src/app/matches/matches.component.ts
+++ Frontend/src/app/matches/matches.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Matches {
+  homeTeam: string;
+  finalResult: string;
+  awayTeam: string;
+}
+
+const matches: Matches[] = [
+  {homeTeam: 'IGTI', awayTeam: 'PUC', finalResult: '2 x 0'},
+  {homeTeam: 'IGTI', awayTeam: 'PUC', finalResult: '2 x 0'},
+  {homeTeam: 'IGTI', awayTeam: 'PUC', finalResult: '2 x 0'},
+  {homeTeam: 'IGTI', awayTeam: 'PUC', finalResult: '2 x 0'},
+  {homeTeam: 'IGTI', awayTeam: 'PUC', finalResult: '2 x 0'},
+  {homeTeam: 'IGTI', awayTeam: 'PUC', finalResult: '2 x 0'},
+  {homeTeam: 'IGTI', awayTeam: 'PUC', finalResult: '2 x 0'},
+  {homeTeam: 'IGTI', awayTeam: 'PUC', finalResult: '2 x 0'}
+]
+
 @Component({
   selector: 'app-matches',
   templateUrl: './matches.component.html',
   styleUrls: ['./matches.component.scss']
 })
 export class MatchesComponent implements OnInit {
+
+  displayedColumns: string[] = ['homeTeam','finalResult', 'awayTeam'];
+  matchesDataSource = matches;
 
   constructor() { }
 
