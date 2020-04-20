436aecfac07428dbec5a1ca29c22f3c380678f85
Artur Quirino <artur.pquirino@gmail.com>
2020-04-20 15:22:15 -0300

    Add row behaviour

--- Frontend/src/app/matches/matches.component.ts
+++ Frontend/src/app/matches/matches.component.ts
@@ -15,7 +15,7 @@ const matches: Matches[] = [
   {homeTeam: 'IGTI', awayTeam: 'PUC', finalResult: '2 x 0'},
   {homeTeam: 'IGTI', awayTeam: 'PUC', finalResult: '2 x 0'},
   {homeTeam: 'IGTI', awayTeam: 'PUC', finalResult: '2 x 0'}
-]
+];
 
 @Component({
   selector: 'app-matches',
@@ -24,12 +24,16 @@ const matches: Matches[] = [
 })
 export class MatchesComponent implements OnInit {
 
-  displayedColumns: string[] = ['homeTeam','finalResult', 'awayTeam'];
+  displayedColumns: string[] = ['homeTeam', 'finalResult', 'awayTeam'];
   matchesDataSource = matches;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  navigateMatchDetails(row): void {
+    console.log(row);
   }
 
 }
