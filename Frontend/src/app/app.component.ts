5d989c10cf0ee0015fe920d57e2f8511a0fa1597
Artur Quirino <artur.pquirino@gmail.com>
2020-05-16 18:11:48 -0300

    Change footer to make more useful and beautifull

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -11,7 +11,28 @@ export class AppComponent {
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
     iconRegistry.addSvgIcon(
       'arrow-back',
-      sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/arrow_back-24px.svg'));
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/arrow_back-24px.svg')
+    );
+
+    iconRegistry.addSvgIcon(
+      'account-box',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/account_box-24px.svg')
+    );
+
+    iconRegistry.addSvgIcon(
+      'assessment',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/assessment-24px.svg')
+    );
+
+    iconRegistry.addSvgIcon(
+      'calendar-view-day',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/calendar_view_day-24px.svg')
+    );
+
+    iconRegistry.addSvgIcon(
+      'keyboard-capslock',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/keyboard_capslock-24px.svg')
+    );
   }
 
   title = 'real-manager';
