a8059edec2aef4b3bd37d1f544d06312c7a1a833
Artur Quirino <artur.pquirino@gmail.com>
2020-06-21 10:58:37 -0300

    Correct unit basic tests

--- Frontend/src/app/matches/matches.component.spec.ts
+++ Frontend/src/app/matches/matches.component.spec.ts
@@ -1,25 +1,41 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { MatchesComponent } from './matches.component';
+import { MatchesComponent } from './matches.component';
+import { MatchApiService } from '../shared/services/match-api.service';
+import { of } from 'rxjs';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../shared/modules/material.module';
+import { MatchComponent } from './match/match.component';
 
-// describe('MatchesComponent', () => {
-//   let component: MatchesComponent;
-//   let fixture: ComponentFixture<MatchesComponent>;
+let matchApiServiceMock: Partial<MatchApiService>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ MatchesComponent ]
-//     })
-//     .compileComponents();
-//   }));
+describe('MatchesComponent', () => {
+  let component: MatchesComponent;
+  let fixture: ComponentFixture<MatchesComponent>;
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(MatchesComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(async(() => {
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    matchApiServiceMock = {
+        getMatchesByTeamId: () => of([])
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [ MatchesComponent ],
+      imports: [ReactiveFormsModule, MaterialModule],
+      providers: [
+          { provide: MatchApiService, useValue: matchApiServiceMock }
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MatchesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
