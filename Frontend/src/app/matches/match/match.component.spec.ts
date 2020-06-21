a71ea2af85015512de3766b154a68ad21ee750d8
Artur Quirino <artur.pquirino@gmail.com>
2020-06-21 11:10:26 -0300

    Fix match test

--- Frontend/src/app/matches/match/match.component.spec.ts
+++ Frontend/src/app/matches/match/match.component.spec.ts
@@ -1,25 +1,43 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { MatchComponent } from './match.component';
+import { MatchComponent } from './match.component';
+import { MatchApiService } from 'src/app/shared/services/match-api.service';
+import { of } from 'rxjs';
+import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
 
-// describe('MatchComponent', () => {
-//   let component: MatchComponent;
-//   let fixture: ComponentFixture<MatchComponent>;
+let matchApiServiceMock: Partial<MatchApiService>;
+let activedRouteMock: Partial<ActivatedRoute>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ MatchComponent ]
-//     })
-//     .compileComponents();
-//   }));
+describe('MatchComponent', () => {
+  let component: MatchComponent;
+  let fixture: ComponentFixture<MatchComponent>;
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(MatchComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(async(() => {
+    matchApiServiceMock = {
+        getMatchById: () => of()
+    };
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    activedRouteMock = new ActivatedRoute();
+    activedRouteMock.snapshot = new ActivatedRouteSnapshot();
+    activedRouteMock.snapshot.paramMap.get = () => '123';
+
+    TestBed.configureTestingModule({
+      declarations: [ MatchComponent ],
+      providers: [
+          { provide: MatchApiService, useValue: matchApiServiceMock },
+          { provide: ActivatedRoute, useValue: activedRouteMock }
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MatchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
