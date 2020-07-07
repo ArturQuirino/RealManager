32a1faaf373dc308c2811e48916ea1d89748657f
Artur Quirino <artur.pquirino@gmail.com>
2020-07-07 08:28:03 -0300

    add body-parser library

--- BackendNode/controllers/leagues.js
+++ BackendNode/controllers/leagues.js
@@ -1,3 +1,11 @@
 module.exports = app => {
-    app.get('/leagues', (req, res) => res.send("you are on route leagues"));
+    app.get('/leagues', (req, res) => {
+        res.send("you are on route leagues")
+    });
+
+    app.post('/leagues', (req, res) => {
+        console.log(req.body);
+        res.send("you are on route leagues")
+    });
+    
 }
